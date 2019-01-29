17bee8b721a6f22a63a5f34349b7615745434601
jyshin0217 <46885452+jyshin0217@users.noreply.github.com>
2019-01-29 23:25:14 +0900

    Add files via upload

---
+++ main1463.java
@@ -0,0 +1,30 @@
+package chapter3;
+
+import java.util.*;
+
+public class main1463 {
+	// 1로 만들기 문제 
+	public int set_one(int n) {
+		int[] count = new int[27];
+		int counter = 0;	
+		while(true) {
+			
+			if(n == 1)
+			{
+				break;
+			}
+		}
+		
+		return count[0];
+	}
+	
+	public static void main(String[] args) {
+		/*
+		 * 문제에서 구할 값을 D[N]에 넣어줘야 한다. -> 즉 연산의 최소값
+		 * D[N] : N =>1 로 만드는데 필요한 연산의 최솟값.
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+}
